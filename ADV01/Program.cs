79ac7f94a0542320c261e5497a3419250acf293d
DareenHazem <dareen.hazem2005@gmail.com>
2026-08-23 10:45:33 +0300

    Question 02

--- ADV01/Program.cs
+++ ADV01/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ADV01
+﻿using System.ComponentModel;
+
+namespace ADV01
 {
     internal class Program
     {
@@ -10,6 +12,15 @@
             // Generics allow you to write classes the actual type is specified when the code is used(compile time) not when it is written
             // and we use it for code reuseability (instead of writing the same code for each type), cleaner code, type safety (catch type mismatches)
             #endregion
+
+            #region Question 02
+            //Container<double> container = new Container<double>();
+            //container.Add(1.568);
+            //container.Add(2.397);
+            //container.Add(3.412);
+            //Console.WriteLine(container.Get(1));
+            #endregion
+
         }
     }
 }
